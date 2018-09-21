6dab4032a347c1d81489ae4c90e860ec53848c62
ShXck <mss97@hotmail.es>
2018-09-21 16:53:31 -0600

    Se agregaron funciones al IDE.

--- IDE/LightBotIDE/src/org/tec/comp/Main.java
+++ IDE/LightBotIDE/src/org/tec/comp/Main.java
@@ -1,12 +1,11 @@
 package org.tec.comp;
 import org.eclipse.paho.client.mqttv3.MqttException;
-
 import javax.swing.*;
+
 
 public class Main {
 
     public static void main(String[] args) throws MqttException {
         SwingUtilities.invokeLater(new IDE_Window());
-
     }
 }
